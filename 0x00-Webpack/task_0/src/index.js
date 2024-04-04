1b00fe0f37b7b2fa51676d363613f47dbe973111
zaragood <azaragoodness52@gmail.com>
2024-04-04 23:25:51 +0100

    added newline

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,8 @@
 import $ from "jquery";
 
 $(function() {
-    var paragraph_1 = $("<p>").text("Holberton Dashboard");
-    var paragraph_2 = $("<p>").text("Dashboard data for the students");
-    var paragraph_3 = $("<p>").text("Copyright - Holberton School");
+    const paragraph_1 = $("<p>").text("Holberton Dashboard");
+    const paragraph_2 = $("<p>").text("Dashboard data for the students");
+    const paragraph_3 = $("<p>").text("Copyright - Holberton School");
     $("body").append(paragraph_1, paragraph_2, paragraph_3)
 })
