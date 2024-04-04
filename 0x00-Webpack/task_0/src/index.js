15b6fbd1752492cae5f962eda1fdb03e79688edf
zaragood <azaragoodness52@gmail.com>
2024-04-04 23:15:36 +0100

    change all p tag

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,8 @@
-import $ from "jQuery";
+import $ from "jquery";
 
 $(function() {
-    var paragraph_1 = $("p").text("Holberton Dashboard");
-    var paragraph_2 = $("p").text("Dashboard data for the students");
-    var paragraph_3 = $("p").text("Copyright - Holberton School");
+    var paragraph_1 = $("<p>").text("Holberton Dashboard");
+    var paragraph_2 = $("<p>").text("Dashboard data for the students");
+    var paragraph_3 = $("<p>").text("Copyright - Holberton School");
     $("body").append(paragraph_1, paragraph_2, paragraph_3)
 })
