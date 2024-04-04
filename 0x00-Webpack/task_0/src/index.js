5b15a802595c5b42a7fb794923f6b58dde4809cf
zaragood <azaragoodness52@gmail.com>
2024-04-04 22:28:51 +0100

    webpack Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+var $ = require( "jquery" );
+
+$(function() {
+    var paragraph_1 = $("p").text("Holberton Dashboard");
+    var paragraph_2 = $("p").text("Dashboard data for the students");
+    var paragraph_3 = $("p").text("Copyright - Holberton School");
+    $("body").append(paragraph_1, paragraph_2, paragraph_3)
+})
