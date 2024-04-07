f33d2c81452ea3f9d4d0f6e17228110ff95d4bd4
zaragood <azaragoodness52@gmail.com>
2024-04-07 23:30:04 +0100

    Embedding expressions, functions

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import logo from './holberton.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="App-header">
+        <div><img src={logo} alt='logo'></img></div>
+        <div><h1>School dashboard</h1></div>
+      </div> 
+      <hr></hr>
+
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      
+      <hr></hr>
+     <div className='App-footer'>
+        <getFullYear/>
+        <getFooterCopy/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Notifications.css';
+
+export default function Notification() {
+    return <div className='Notifications'><p>Here is the list of notifications</p></div>
+}
